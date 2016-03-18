c98a417daac878b9474d6bc984cceb66d455784e
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-03-18 13:16:06 +0530

    fect : Created class for Fibonacci
    Added Logic for finding Fibonacci

---
+++ Fibonacci.py
@@ -0,0 +1,17 @@
+class Fibonacci:
+    sumFibo = 0
+
+    def __init__(self, n):
+        first = 0
+        second = 1
+        current = 0
+        while current < n:
+            current = first + second
+            if current % 2 == 0:
+                self.sumFibo += current
+
+            first = second
+            second = current
+
+    def showFibo(self):
+        print self.sumFibo
