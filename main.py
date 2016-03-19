c32b583c314aa849f221f8a28fbef805368185e9
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-03-19 15:01:04 +0530

    fect : Added logic to create and check Palindrom

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from source import Multiply as mul
 
 from source import Fibonacci as fibo
 
-mul.Multiple(1000).showMultiple()
-mul.Multiple(100).showMultiple()
-fibo.Fibonacci(4000000).showFibo()
+# mul.Multiple(1000).showMultiple()
+# mul.Multiple(100).showMultiple()
+# fibo.Fibonacci(4000000).showFibo()
+#
+# primeNumber.LargestPrime().displayPrime()
 
-primeNumber.LargestPrime().displayPrime()
+
--- source/Palindrom.py
+++ source/Palindrom.py
@@ -5,3 +5,18 @@ class Palindrom:
         for i in range(999, 100):
             for j in range(999, 100):
                 self.largestPrime = i * j
+
+        print(self.isPalindrom(1001))
+
+    def displayPrime(self):
+        print(self.largestPrime)
+
+    def isPalindrom(self, num):
+        reverse = 0
+        orignal = num
+        remainder = 0
+        while num > 0:
+            remainder = num % 10
+            reverse = reverse * 10 + remainder
+            num = num / 10
+        return reverse == orignal
