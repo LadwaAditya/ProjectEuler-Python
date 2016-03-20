d2d05a306d74918c0c6c42bce50e46d74aa59a6a
Aditya Ladwa <ladwa.aditya@gmail.com>
2016-03-20 16:33:44 +0530

    fect : Finished Problem 05
    Passing Tests

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from source import LargestPrime as primeNumber
 from source import Multiply as mul
 from source import Fibonacci as fibo
 from source import Palindrom as palin
+from source import SmallestMultiple as small
 
 # mul.Multiple(1000).showMultiple()
 # mul.Multiple(100).showMultiple()
@@ -9,4 +10,6 @@ from source import Palindrom as palin
 #
 # primeNumber.LargestPrime().displayPrime()
 
-palin.Palindrom().displayPrime();
+# palin.Palindrom().displayPrime()
+
+small.SmallestMultiple().printMultiple()
--- source/SmallestMultiple.py
+++ source/SmallestMultiple.py
@@ -6,12 +6,16 @@ class SmallestMultiple:
         flag = 0
         while n:
             for i in range(2, 20):
-                flag = n % i == 0
-                if flag != 1:
+                if n % i == 0:
+                    flag = 1
+                else:
+                    flag = 0
+                if flag == 0:
                     break
             if flag == 1:
                 self.smallestMultiple = n
                 break
+            n += 1
 
     def printMultiple(self):
         print(self.smallestMultiple)
